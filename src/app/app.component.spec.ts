e62b9e2355e541fdd8bbc98e9a599f9eeb93d286
Hi-Dukka <dukka.cua@gmail.com>
2024-02-05 18:49:12 +0530

    test client

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Git app is runn');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('git app is running');
   });
 });
