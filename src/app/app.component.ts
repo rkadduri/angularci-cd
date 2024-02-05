4f311d394c7bb01343ff5fff0bae342041b9b4a5
Hi-Dukka <dukka.cua@gmail.com>
2024-02-05 12:00:23 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,89 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-class';
+  bool:boolean = false;
+
+
+
+   //onkeyup: any;
+
+
+//userName:string | undefined;
+
+//textValue: string | undefined;
+
+
+// string interpolution data binding
+/*
+buttonClick(){
+console.log('button click Event worked')
+
+  }
+  */
+
+
+
+// Leanth method
+
+
+//  onKeyup($event: any) {
+// if($event.keyCode == 13) {
+// console.log('Enter Key is preessed ');
+// }
+//   }
+
+// Easy methods
+/*
+onKeyup() {
+  console.log('Enter key is Pressed')
+}*/
+
+
+// old opproach
+/*
+onKeyup($event:any) {
+  console.log($event.target.value)
+}
+*/
+
+
+
+// Clean approach
+/*
+onKeyup(username: any) {
+  console.log(username)
+}
+*/
+
+// Two way data binding goes here
+/*
+onKeyup() {
+  console.log(this.userName)
+}
+*/
+/*
+onKeyup() {
+  console.log(this.textValue)
+}
+*/
+
+/*
+
+My assignment 1
+
+modeltitle: string;
+postdes: string;
+urlpost:any;
+linkurl:any;
+value: string = 'Joe';
+imageToShow: any;
+*/
+
+
+PostTitle:string;
+PostDetails:string;
+imageURL:string;
+postURL:string;
+addBackground:boolean;
+
 }
